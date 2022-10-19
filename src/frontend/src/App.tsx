bf06fc3f769a05f7c7e128efee09c7485dcf2bfb
Sam Monballiu <samsonmonballiu@hotmail.com>
2022-10-19 16:33:47 +0200

    frontend: add useQuery and axios

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { Chore } from "@common/models";
+import Button from "@mui/material/Button";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React, { FC } from "react";
 import "./App.scss";
 
+const url = "http://raspberrypi:1904/api/chores/";
+
 const App: FC = () => {
+  const { data: chores } = useQuery(["getChores"], async () => {
+    return await axios.get<Chore[]>(url);
+  });
+
   return (
     <div className="App">
-      <h2>Hello world</h2>
+      <h1>Hello world</h1>
+      <Button variant="contained">Test</Button>
     </div>
   );
 };
--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -2,13 +2,24 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.scss";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
