b3c2a50918e5b743cd08fd9b2c2975e993da6f10
SamMonballiu <samsonmonballiu@hotmail.com>
2022-10-23 16:13:22 +0200

    show repeat interval for inactive chores

--- src/frontend/src/models/settings.ts
+++ src/frontend/src/models/settings.ts
@@ -1,7 +1,10 @@
 interface ISettings {
   baseUrl: string;
+  showRepeatIntervalOnInactiveChores: boolean;
 }
 
 export const Settings: ISettings = {
   baseUrl: import.meta.env.VITE_URL as string,
+  showRepeatIntervalOnInactiveChores:
+    (import.meta.env.VITE_SHOW_INTERVAL_FOR_INACTIVE as boolean) ?? false,
 };
